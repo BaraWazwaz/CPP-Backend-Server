edd40728a2238e30aa2a54e9b9bf6a9759833f40
BaraWazwaz <wazwazbaraa@gmail.com>
2026-09-11 16:57:31 +0300

    runs smoothly now

--- runner.sh
+++ runner.sh
@@ -1,22 +1,25 @@
 #!/usr/bin/env bash
 
 if [[ $# -ne 1 ]]; then
-    echo -e "error: expected \"run\" or \"compile\""
+    echo -e "error: expected [\"run\", \"build\", \"clean\"]"
     exit 1
 fi
 
 case "$1" in
     'run')
-        if [[ ! -f main.exe ]]; then
-            g++ main.cpp -o main.exe
+        if [[ ! -f ./build/MyServer.exe ]]; then
+            ./runner.sh build
         fi
-        ./main.exe
+        PATH="/c/msys64/ucrt64/bin:$PATH" ./build/MyServer.exe
         ;;
-    'compile')
-        g++ main.cpp -o main.exe
+    'build')
+        cmake --build ./build
+        ;;
+    'clean')
+        rm -fr ./build
         ;;
     *)
-        echo -e "error: expected \"run\" or \"compile\""
+        echo -e "error: expected \"run\" or \"build\""
         exit 1
         ;;
 esac
